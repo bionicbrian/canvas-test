b5604bb100a7b9d29f9c0fc8d6476db0c4da9bfe
Brian Moore <bmoore@book.com>
2014-11-25 19:06:59 -0500

    w00t. update new position with random num

--- src/canvas_test/core.cljs
+++ src/canvas_test/core.cljs
@@ -31,21 +31,22 @@
 (defn split-distance [current new]
   (+ current (* (- new current) 0.1)))
 
+(defn move-distance [m k]
+  (if (is-greater-than-threshold? (k m))
+      (update-in m [k :current] split-distance (get-in m [k :new]))
+      (update-in m [k :new] #(rand 500))))
+
 (defn update-actor [{:keys [x y] :as actor}]
-  (reduce
-    (fn [m k] (update-in m [k :current] split-distance (get-in m [k :new])))
-    actor
-    (keys (select-keys actor [:x :y]))))
+  (reduce move-distance actor (keys (select-keys actor [:x :y]))))
 
 (defn update [actors]
   (map #(update-actor %) actors))
 
 (defn loop-game [canvas ctx actors]
   (let [new-actors (update actors)]
-    (if (should-update? new-actors)
-      (do
-        (render canvas ctx new-actors)
-        (.requestAnimationFrame js/window (partial loop-game canvas ctx new-actors))))))
-        
+    (do
+      (render canvas ctx new-actors)
+      (.requestAnimationFrame js/window (partial loop-game canvas ctx new-actors)))))
+
 (render canvas ctx actors)
 (loop-game canvas ctx actors)
